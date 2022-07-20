29e11349bc638212f3ad1ea17a602dbe29ed8914
steven.rogiers <srogiers@win.ips-ag.net>
2022-07-20 16:04:05 +0200

    Home page section cards

--- src/pwa/src/views/Home.js
+++ src/pwa/src/views/Home.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import CardData from "../data/cardData.json";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Button from 'react-bootstrap/Button';
+
 
 function Home() {
 
@@ -6,6 +11,22 @@ function Home() {
     <div>
       <h4>Home</h4>
       <div className="cards-section">
+        {CardData.map((card,i) => {
+          return (
+            <CardGroup key={i}>
+              <Card >
+                <Card.Img variant="top" src={card.itemImage} />
+                <Card.Body>
+                  <Card.Title>{card.title}</Card.Title>
+                  <Card.Text>
+                    {card.itemText}
+                  </Card.Text>
+                  <Button variant="outline-primary" color="secondary" href={card.buttonUrl} >{card.itemButtonText}</Button>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+          )
+        })}
 
       </div>
     </div>
