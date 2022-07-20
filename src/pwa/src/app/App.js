7eb7a685544c7bb75687f923ea4c47a1ca1e2289
steven.rogiers <srogiers@win.ips-ag.net>
2022-07-20 15:16:02 +0200

    restructure app folder - react hot reload - add babel

---
+++ src/pwa/src/app/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+import './App.scss';
+
+
+function App() {
+
+  return (
+    <>
+     PWA home page
+    </>
+  );
+  
+}
+
+export default hot(module)(App);
+
