0a5d76da5eaa3aa73a6710a24de666dfa797d56f
Miletić Uroš <uros.miletic@ips-ag.com>
2022-07-29 10:55:56 +0200

    Generate static content

--- src/pwa/next.config.js
+++ src/pwa/next.config.js
@@ -1,6 +1,11 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
   pwa: {
     dest: "public",
     register: true,
