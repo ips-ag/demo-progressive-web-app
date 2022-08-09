8267a8fba1266f160d15914fdab94a0ccf15e0d8
linh.khanh_nguyen_c <linh.khanh_nguyen_c@sandvik.com>
2022-08-09 17:13:33 +0700

    add marker as current location

--- src/pwa/pages/maps.tsx
+++ src/pwa/pages/maps.tsx
@@ -1,12 +1,16 @@
-import { Grid, Typography } from '@mui/material';
-import GoogleMapReact from 'google-map-react';
+import { Avatar, Box, Card, CardContent, Grid, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
+import GoogleMap from 'google-map-react';
 import { useEffect, useState } from "react";
+import MarkerPosition from '../components/partials/MarkerPosition';
+import PersonPinCircleIcon from '@mui/icons-material/PersonPinCircle';
+
 const Map = () => {
     const [currentGeoPosition, setCurrentGeoPosition] = useState<any>();
+    const [currentZoom, setCurrentZoom] = useState<number>(15);
 
     const getCurrentPosition = () => {
+        setCurrentZoom(15)
         navigator.geolocation.getCurrentPosition((position) => {
-            console.info(position)
             setCurrentGeoPosition(position)
         });
     }
@@ -16,25 +20,39 @@ const Map = () => {
     }, []);
 
     return (
-        <div style={{ height: '100vh', width: '100%' }}>
+        <>
             {currentGeoPosition ?
-                <GoogleMapReact
-                    defaultCenter={{
-                        lat: currentGeoPosition.coords.latitude,
-                        lng: currentGeoPosition.coords.longitude
-                    }}
-                    defaultZoom={15}
-                    yesIWantToUseGoogleMapApiInternals
-                >
+                <Box sx={{ display: { md: 'flex' } }}>
+                    <Card sx={{ width: '100%', height: '90vh' }}>
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <PersonPinCircleIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Current location" secondary={`${currentGeoPosition.coords.latitude} : ${currentGeoPosition.coords.longitude}`} />
+                        </ListItem>
+                        <CardContent sx={{ width: '100%', height: '90%' }}>
+                            <GoogleMap
+                                yesIWantToUseGoogleMapApiInternals={true}
+                                center={{
+                                    lat: currentGeoPosition.coords.latitude,
+                                    lng: currentGeoPosition.coords.longitude
+                                }}
+                                zoom={currentZoom}
+                            >
 
-                </GoogleMapReact>
+                                <MarkerPosition lat={currentGeoPosition.coords.latitude} lng={currentGeoPosition.coords.longitude} text="Your location" />
+                            </GoogleMap>
+                        </CardContent>
+                    </Card>
+                </Box>
                 :
                 <Grid sx={{ padding: 3 }} container justifyContent="center" >
                     <Typography color='error'>Your device is not support GPS or you need to grant permission to access GPS for this page. </Typography>
                 </Grid>
             }
-
-        </div>
+        </>
     )
 }
 
