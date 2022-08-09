c94dd4f7d30b31f03a02364470611348a37065f4
linh.khanh_nguyen_c <linh.khanh_nguyen_c@sandvik.com>
2022-08-09 08:23:00 +0700

    build the map feature

--- src/pwa/pages/maps.tsx
+++ src/pwa/pages/maps.tsx
@@ -1,6 +1,40 @@
+import { Grid, Typography } from '@mui/material';
+import GoogleMapReact from 'google-map-react';
+import { useEffect, useState } from "react";
 const Map = () => {
+    const [currentGeoPosition, setCurrentGeoPosition] = useState<any>();
+
+    const getCurrentPosition = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.info(position)
+            setCurrentGeoPosition(position)
+        });
+    }
+
+    useEffect(() => {
+        getCurrentPosition();
+    }, []);
+
     return (
-        <h4>Map</h4>
+        <div style={{ height: '100vh', width: '100%' }}>
+            {currentGeoPosition ?
+                <GoogleMapReact
+                    defaultCenter={{
+                        lat: currentGeoPosition.coords.latitude,
+                        lng: currentGeoPosition.coords.longitude
+                    }}
+                    defaultZoom={15}
+                    yesIWantToUseGoogleMapApiInternals
+                >
+
+                </GoogleMapReact>
+                :
+                <Grid sx={{ padding: 3 }} container justifyContent="center" >
+                    <Typography color='error'>Your device is not support GPS or you need to grant permission to access GPS for this page. </Typography>
+                </Grid>
+            }
+
+        </div>
     )
 }
 
