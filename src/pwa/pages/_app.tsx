c58a2baf701ccf24a4fd9a057c9f1b6f742f85de
linhnk-ips <109565148+linhnk-ips@users.noreply.github.com>
2022-08-15 17:36:50 +0700

    Add progress indicator during app load (#34)
    * add loading progress when render app and page
    
    * dynamic import with layout
    
    Co-authored-by: linh.khanh_nguyen_c <linh.khanh_nguyen_c@sandvik.com>

--- src/pwa/pages/_app.tsx
+++ src/pwa/pages/_app.tsx
@@ -7,10 +7,11 @@ import '@fontsource/roboto/700.css';
 import * as React from 'react';
 import type { AppProps } from 'next/app';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import { ThemeProvider, createTheme, Grid, CircularProgress } from '@mui/material';
 import createEmotionCache from '../utility/createEmotionCache';
 import lightThemeOptions from '../styles/theme/lightThemeOptions';
-import Layout from '../components/Layout'
+import { Router } from 'next/router';
+import dynamic from 'next/dynamic';
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -19,16 +20,44 @@ const clientSideEmotionCache = createEmotionCache();
 
 const lightTheme = createTheme(lightThemeOptions);
 
+const Layout = dynamic(() => import('../components/Layout'), {
+  ssr: false
+})
+
 const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  Router.events.on('routeChangeStart', (url) => {
+    setLoading(true);
+  });
+
+  Router.events.on('routeChangeComplete', (url) => {
+    setLoading(false);
+  });
 
   return (
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
         <Layout>
-          <Component {...pageProps} />
+          {loading ? (
+            <Grid
+              container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              style={{ minHeight: '100vh' }}
+            >
+              <Grid item xs={3}>
+                <CircularProgress color='info' />
+              </Grid>
+            </Grid>
+          ) : (
+            <Component {...pageProps} />
+          )}
         </Layout>
+
       </ThemeProvider>
     </CacheProvider>
   );
